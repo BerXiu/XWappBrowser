4bd738a26ce5ceac42a71c9d8166be4f59fa3116
Xiu <gogs@fake.local>
2016-08-26 13:41:20 +0800

    first commit

---
+++ XWappBrowser/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XWappBrowser
+//
+//  Created by Xiu on 16/8/26.
+//  Copyright © 2016年 Xiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
